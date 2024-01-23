2a4635056d88d9e3f4f782c8e80d91a280ce96e0
Amaresh Sahoo <amareshsahoo441@gmail.com>
2024-01-23 12:14:39 +0530

    Saloonify Website

---
+++ src/HomeImage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import IMAGE from "./assets/image.png"
+
+const HomeImage = () => {
+  return(
+    <div className='homeimage'>
+      <img src={IMAGE} alt="" />
+      <p>Book your</p>
+      <p>Favourite Time</p>
+
+    </div>
+  )
+}
+export default HomeImage 
